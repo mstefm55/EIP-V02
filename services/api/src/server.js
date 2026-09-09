98812e180a96819967b176ae609f63767e92d8b0
mstefm55 <mstefm55@gmail.com>
2026-09-09 16:21:51 +0400

    feat(connections): register tenant target routes

--- services/api/src/server.js
+++ services/api/src/server.js
@@ -20,6 +20,7 @@ import planningScheduleRoutes from "./routes/planning_schedule.js";
 import uiSurfaceRoutes from "./routes/ui_surface.js";
 import ownerAdminConsoleRoutes from "./routes/owner_admin_console.js";
 import connectionRoutes from "./routes/connections.js";
+import connectionTargetRoutes from "./routes/connections_target.js";
 import connectionEndpointRoutes from "./routes/connections_endpoints.js";
 import connectionReadinessRoutes from "./routes/connections_readiness.js";
 import { advanceInstance, createInstance, findActiveInstance, updateTaskStatus } from "./core/core_process_engine.js";
@@ -233,6 +234,7 @@ async function buildServer(options = {}) {
   await app.register(uiSurfaceRoutes, { prefix: "/api/eip" });
   await app.register(ownerAdminConsoleRoutes, { prefix: "/api/eip" });
   await app.register(connectionRoutes, { prefix: "/api/eip" });
+  await app.register(connectionTargetRoutes, { prefix: "/api/eip" });
   await app.register(connectionEndpointRoutes, { prefix: "/api/eip" });
   await app.register(connectionReadinessRoutes, { prefix: "/api/eip" });
   await app.register(planningScheduleRoutes, { prefix: "/api/eip" });
